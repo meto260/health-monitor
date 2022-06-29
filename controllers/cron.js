4d8c43915971ec9c959b6482a9ec8535bb16211b
Metin <meto260@gmail.com>
2022-06-29 18:29:00 +0300

    add routes before database operations

---
+++ controllers/cron.js
@@ -0,0 +1,16 @@
+module.exports.ListJobs = (q, s) => {
+    return s.json({status:1});
+}
+
+module.exports.AddJob = (q, s) => {
+    return s.json({status:1});
+}
+
+module.exports.UpdateJob = (q, s) => {
+    return s.json({status:1});
+}
+
+module.exports.RemoveJob = (q, s) => {
+    return s.json({status:1});
+}
+
